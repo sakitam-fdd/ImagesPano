e39db814ce885eaa86864317e3b82f6cb2df4413
FDD <1253148931@qq.com>
2017-07-15 22:48:09 +0800

    # FIX event

--- src/events/Observable.js
+++ src/events/Observable.js
@@ -1,182 +1,129 @@
-class Observable {
-  on (events, callback) {
-    this.__events = this.__events || {}
-    if (typeof events === 'object') {
-      for (let event in events) {
-        if (events.hasOwnProperty(event)) {
-          this.__events[event] = this.__events[event] || []
-          this.__events[event].push(events[event])
-        }
-      }
-    } else {
-      events.split(' ').forEach(function (event) {
-        this.__events[event] = this.__events[event] || []
-        this.__events[event].push(callback)
-      }, this)
+let Events = {}
+let __cnt = 0
+/**
+ * 事件分发
+ * @param eventName
+ * @param callback
+ * @param context
+ * @returns {(*|*)[]}
+ */
+const on = (eventName, callback, context) => {
+  return bindEvent(eventName, callback, 0, context)
+}
+/**
+ * 取消监听
+ * @param event
+ * @returns {boolean}
+ */
+const un = (event) => {
+  let [eventName, key, r, type] = ['', '', false, (typeof event)]
+  if (type === 'string') {
+    if (hasOwnKey(Events, event)) {
+      delete Events[event]
+      return true
     }
-    return this
-  }
-
-  un (events, callback) {
-    this.__events = this.__events || {}
-    if (typeof events === 'object') {
-      for (let event in events) {
-        if (events.hasOwnProperty(event) && (event in this.__events)) {
-          let index = this.__events[event].indexOf(events[event])
-          if (index !== -1) this.__events[event].splice(index, 1)
-        }
-      }
-    } else if (!!events) {
-      events.split(' ').forEach(function (event) {
-        if (event in this.__events) {
-          if (callback) {
-            let index = this.__events[event].indexOf(callback)
-            if (index !== -1) this.__events[event].splice(index, 1)
-          } else {
-            this.__events[event].length = 0
-          }
-        }
-      }, this)
-    } else {
-      this.__events = {}
+    return false
+  } else if (type === 'object') {
+    eventName = event[0]
+    key = event[1]
+    if (hasOwnKey(Events, eventName) && hasOwnKey(Events[eventName], key)) {
+      delete Events[eventName][key]
+      return true
     }
-    return this
-  }
-
-  once (events, callback) {
-    this.__once = this.__once || {}
-    if (typeof events === 'object') {
-      for (let event in events) {
-        if (events.hasOwnProperty(event)) {
-          this.__once[event] = this.__once[event] || []
-          this.__once[event].push(events[event])
+    return false
+  } else if (type === 'function') {
+    eachEvent(Events, function (keyA, itemA) {
+      eachEvent(itemA, function (keyB, itemB) {
+        if (itemB[0] === event) {
+          delete Events[keyA][keyB]
+          r = true
         }
-      }
-    } else {
-      events.split(' ').forEach(function (event) {
-        this.__once[event] = this.__once[event] || []
-        this.__once[event].push(callback)
-      }, this)
-    }
-    return this
-  }
-
-  trigger (event) {
-    let args = Array.prototype.slice.call(arguments, 1)
-    let e = new uEvent.Event(event, args)
-    var i, l, f;
-    args.push(e)
-    if (this.__events && event in this.__events) {
-      for (i = 0, l = this.__events[event].length; i < l; i++) {
-        f = this.__events[event][i]
-        if (typeof f === 'object') {
-          f.handleEvent(e)
-        } else {
-          f.apply(this, args)
-        }
-        if (e.isPropagationStopped()) {
-          return e
-        }
-      }
-    }
-    if (this.__once && event in this.__once) {
-      for (i = 0, l = this.__once[event].length; i < l; i++) {
-        f = this.__once[event][i];
-        if (typeof f === 'object') {
-          f.handleEvent(e)
-        } else {
-          f.apply(this, args)
-        }
-        if (e.isPropagationStopped()) {
-          delete this.__once[event]
-          return e
-        }
-      }
-      delete this.__once[event]
-    }
-    return e
-  }
-
-  change (event, value) {
-    var args = Array.prototype.slice.call(arguments, 1)
-    var e = new uEvent.Event(event, args)
-    var i, l, f
-
-    args.push(e)
-
-    if (this.__events && event in this.__events) {
-      for (i = 0, l = this.__events[event].length; i < l; i++) {
-        args[0] = value
-        f = this.__events[event][i]
-        if (typeof f === 'object') {
-          value = f.handleEvent(e)
-        } else {
-          value = f.apply(this, args)
-        }
-        if (e.isPropagationStopped()) {
-          return value
-        }
-      }
-    }
-
-    return value;
-  }
-
-  static Event = function (type, args) {
-    let typeReadOnly = type
-    let argsReadonly = args
-    Object.defineProperties(this, {
-      'type': {
-        get: function () {
-          return typeReadOnly
-        },
-        set: function (value) {
-        },
-        enumerable: true
-      },
-      'args': {
-        get: function () {
-          return argsReadonly
-        },
-        set: function (value) {
-        },
-        enumerable: true
-      }
+      })
     })
-    this.isDefaultPrevented = false
-    this.isPropagationStopped = true
-    this.preventDefault = function () {
-      this.isDefaultPrevented = true
-    }
-    this.stopPropagation = function () {
-      this.isPropagationStopped = true
-    }
+    return r
   }
-
-  static mixin = function (target, names) {
-    names = names || {}
-    target = (typeof target === 'function' ? target.prototype : target)
-    (['on', 'off', 'once', 'trigger', 'change']).forEach(function (name) {
-      let method = names[name] || name;
-      target[method] = uEvent.prototype[name]
-    })
-    Object.defineProperties(target, {
-      '__events': {
-        value: null,
-        writable: true
-      },
-      '__once': {
-        value: null,
-        writable: true
+  return true
+}
+/**
+ * 事件监听（只触发一次）
+ * @param eventName
+ * @param callback
+ * @param context
+ * @returns {(*|*)[]}
+ */
+const once = (eventName, callback, context) => {
+  return bindEvent(eventName, callback, 1, context)
+}
+/**
+ * 响应事件
+ * @param eventName
+ * @param args
+ */
+const action = (eventName, args) => {
+  if (hasOwnKey(Events, eventName)) {
+    eachEvent(Events[eventName], function (key, item) {
+      item[0].apply(item[2], args)
+      if (item[1]) {
+        delete Events[eventName][key]
       }
     })
   }
-  static unByKey = function (key) {
-    if (Array.isArray(key)) {
-      for (var i = 0, ii = key.length; i < ii; ++i) {
-      }
-    } else {
+}
+/**
+ * 实时触发响应
+ * @param eventName
+ */
+const dispatch = (eventName) => {
+  let args = slice(arguments, 1)
+  setTimeout(function () {
+    action(eventName, args)
+  })
+}
+/**
+ * 延后触发响应
+ * @param eventName
+ */
+const dispatchSync = (eventName) => {
+  action(eventName, slice(arguments, 1))
+}
+/**
+ * 清空发布中心
+ */
+const clear = () => {
+  Events = {}
+}
+/**
+ * 绑定事件
+ * @param eventName
+ * @param callback
+ * @param isOne
+ * @param context
+ * @returns {[*,*]}
+ */
+const bindEvent = (eventName, callback, isOne, context) => {
+  if (typeof eventName !== 'string' || typeof callback !== 'function') {
+    throw new Error('传入的事件名称和回调函数有误！')
+  }
+  if (!hasOwnKey(Events, eventName)) {
+    Events[eventName] = {}
+  }
+  Events[eventName][++__cnt] = [callback, isOne, context]
+  return [eventName, __cnt]
+}
+const hasOwnKey = Function.call.bind(Object.hasOwnProperty)
+const slice = Function.call.bind(Array.prototype.slice)
+const eachEvent = (obj, callback) => {
+  for (let key in obj) {
+    if (hasOwnKey(obj, key)) {
+      callback(key, obj[key])
     }
   }
 }
-export default Observable
+export default {
+  on,
+  once,
+  un,
+  dispatch,
+  dispatchSync,
+  clear
+}
